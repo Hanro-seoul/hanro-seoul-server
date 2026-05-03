c1347db81332368e79cf219aee5e6d222f2e6d3a
jinsung <hanro101707@gmail.com>
2026-05-04 06:30:49 +0900

    docker-test

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,9 +22,11 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    runtimeOnly("org.postgresql:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
---
+++ src/main/kotlin/com/hanro/server/TestController.kt
@@ -0,0 +1,7 @@
+package com.hanro.server
+
+import org.springframework.web.bind.annotation.RequestMapping
+
+@RequestMapping
+class TestController {
+}
